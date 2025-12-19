28ef4901049c78cd72e7373a1371e1106733bea6
anki <anki007123@gmail.com>
2025-12-19 17:58:57 +0100

    day 2

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,53 @@
 const express = require("express");
+
 const app = express();
 
+// in memory storage
+const orders = {};
+const transactions = {};
+
+// allow JSON body
+app.use(express.json());
+
+// health check
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
+
+// create order
+app.post("/order/create", (req, res) => {
+  const { amount } = req.body;
+
+  const orderId = "ORD_" + Date.now();
+
+  orders[orderId] = {
+    orderId,
+    amount,
+    status: "CREATED",
+  };
+
+  res.json({
+    orderId,
+    amount,
+    status: "CREATED",
+  });
+});
+
+app.post("/payment/manual", (req, res) => {
+  const { orderId } = req.body;
+
+  if (!orders[orderId]) {
+    return res.status(404).json({ error: "Order not found" });
+  }
+
+  orders[orderId].status = "PAID";
+
+  res.json({
+    orderId,
+    status: "SUCCESS",
+  });
+});
+
 
 app.listen(3001, () => {
   console.log("Backend running on port 3001");
